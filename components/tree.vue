7d6a09668a4da8251e51b51109edba0e09043ea7
Mohammad Bagher Abedi Sagha <mbapost@yahoo.com>
2021-08-25 02:57:23 +0430

    vuetify accordion added

--- components/tree.vue
+++ components/tree.vue
@@ -1,17 +1,44 @@
 <template>
-  <ul>
+  <!-- <ul>
     <li v-for="leave in data" :key="leave.title">
-      <leave :data="leave"></leave>
     </li>
-  </ul>
+  </ul> -->
+
+  <v-expansion-panels v-model="opens" multiple>
+    <v-expansion-panel v-for="(leave, i) in data" :key="i" multiple>
+      <v-expansion-panel-header>
+        <v-text-field
+          :value="leave.title"
+          @click.stop
+          outlined
+          dense
+          class="textf"
+        ></v-text-field>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <tree :data="leave.children" v-if="leave.children.length"></tree>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script>
 export default {
   name: "tree",
   props: ["data"],
+  data: () => {
+    return {
+      opens: [],
+    };
+  },
 };
 </script>
 
 <style>
+.textf fieldset {
+  border: none;
+}
+</style>
+
+<style scoped>
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,29 @@
 <template>
-	<v-row justify="center" align="center">
-		<v-col cols="12" sm="8" md="6">
-			<v-card outlined class="pa-5 pt-0">
-				<v-card-title>
-					<span class="font-weight-black">baQ</span>log</v-card-title
-				>
-				<v-text-field outlined style="border: none">title</v-text-field>
-				<v-card elevation="0">
-					<v-textarea
-						class="pb-0"
-						outlined
-						auto-grow
-						hide-details="true"
-						rows="1"
-						row-height="40"
-					>
-						f
-					</v-textarea>
-				</v-card>
-			</v-card>
-		</v-col>
-	</v-row>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card outlined class="pa-5 pt-0">
+        <v-card-title>
+          <span class="font-weight-black">baQ</span>log</v-card-title
+        >
+        <v-text-field outlined style="border: none">title</v-text-field>
+        <v-card elevation="0">
+          <v-textarea
+            class="pb-0"
+            outlined
+            auto-grow
+            hide-details="true"
+            rows="1"
+            row-height="40"
+          >
+            f
+          </v-textarea>
+        </v-card>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
+
+
+<script>
+export default {};
+</script>
